bfc5b282e4a710a908c2fd0dc806d77302605530
Bas Donker <donkerbc@gmail.com>
2022-07-02 17:29:00 +0100

    Create boilerplate template

---
+++ src/NavBar/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <a href="/">
+      <h1>VoteBee</h1>
+    </a>
+  )
+}
+
+export default NavBar
---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+import firebase from "firebase/app"
+import "firebase/database"
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: REACT_APP_FIREBASE_PRODUCT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: "G-DD0MTEN056"
+};
+
+firebase.initializeApp(firebaseConfig)
+const databaseRef = firebase.database.ref()
+
+export const appRef = databaseRef.child("app")
+export default firebase
