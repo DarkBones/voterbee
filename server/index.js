f24af336eff55f6acfa7da6d05d3fa65fa474633
Bas Donker <donkerbc@gmail.com>
2022-07-23 15:47:29 +0100

    Initialize firebase

--- server/index.js
+++ server/index.js
@@ -1,10 +1,18 @@
+require('dotenv').config()
 const express = require('express')
 
 const PORT = process.env.PORT || 3001
-
 const app = express()
-
 const baseUrl = '/api/v1'
+const {
+  initializeApp,
+  applicationDefault,
+} = require('firebase-admin/app')
+
+initializeApp({
+  credential: applicationDefault(),
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+})
 
 app.get(baseUrl, (req, res) => {
   res.json({ message: 'Hello from server!' })
