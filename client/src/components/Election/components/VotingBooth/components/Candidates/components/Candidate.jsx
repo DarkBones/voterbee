f1aa635bcb73ccbf37e271385dc27362ccf82175
Bas Donker <bas.donker@forma.ai>
2022-08-10 11:52:55 +0100

    Style candidate

--- client/src/components/Election/components/VotingBooth/components/Candidates/components/Candidate.jsx
+++ client/src/components/Election/components/VotingBooth/components/Candidates/components/Candidate.jsx
@@ -1,11 +1,25 @@
 import PropTypes from 'prop-types'
+import { Grid } from 'shared/components'
 import style from './Candidate.module.scss'
 
 function Candidate({ candidate, index }) {
-  console.log(index)
   return (
-    <div className={style.candidate}>
-      {candidate.name}
+    <div className={style.candidate_container}>
+      <div className={style.candidate}>
+        <Grid container className={style.grid_container}>
+          <Grid className={style.index_box} alignItems="center">
+            <div className={style.index_number}>
+              {index}
+            </div>
+          </Grid>
+          <Grid xs className={style.candidate_value}>
+            {candidate.name}
+          </Grid>
+          <Grid className={style.switch}>
+            S
+          </Grid>
+        </Grid>
+      </div>
     </div>
   )
 }
